5481bca8ea855c9a9440074543663fe5d0f71aac
zfybs <619086871@qq.com>
2016-06-13 15:06:31 +0800

    添加发送鼠标消息的API

--- Projects/eZAPI/Collection/APIMessage.vb
+++ Projects/eZAPI/Collection/APIMessage.vb
@@ -1,8 +1,8 @@
 ﻿Imports System.Runtime.InteropServices
 Public Class APIMessage
 
-    Public Delegate Function WndProcDelegate(ByVal hWnd As IntPtr, _
-                                      ByVal msg As UInteger, ByVal wParam As IntPtr, _
+    Public Delegate Function WndProcDelegate(ByVal hWnd As IntPtr,
+                                      ByVal msg As UInteger, ByVal wParam As IntPtr,
                                       ByVal lParam As IntPtr) As IntPtr
     ''' <summary>
     ''' 将指定消息信息传送给指定的窗口过程。
@@ -16,9 +16,9 @@ Public Class APIMessage
     ''' <param name="lParam">指定其余的、消息特定的信息。该参数的内容与Msg参数值有关。</param>
     ''' <returns>返回值指定了消息处理结果，它与发送的消息有关。</returns>
     ''' <remarks></remarks>
-    <DllImport("user32.dll")> _
-    Public Shared Function CallWindowProc(lpPrevWndFunc As WndProcDelegate, _
-                                           hWnd As IntPtr, Msg As UInteger, _
+    <DllImport("user32.dll")>
+    Public Shared Function CallWindowProc(lpPrevWndFunc As WndProcDelegate,
+                                           hWnd As IntPtr, Msg As UInteger,
                                            wParam As IntPtr, lParam As IntPtr) As IntPtr
         'CallWindowProc与DefWindowProc关键的区别在于：
         'CallWindowProc 是发送某个消息给某个处理函数(包括自定义消息处理函数),  而DefWindowProc 是发送某个消息给Windows默认的消息处理函数.
@@ -38,9 +38,9 @@ Public Class APIMessage
     ''' <param name="lParam">指定其余的、消息特定的信息。该参数的内容与Msg参数值有关。</param>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    <DllImport("user32.dll")> _
-    Public Shared Function DefWindowProc(ByVal hWnd As IntPtr, _
-                                         ByVal uMsg As WindowsMessages, ByVal wParam As IntPtr, _
+    <DllImport("user32.dll")>
+    Public Shared Function DefWindowProc(ByVal hWnd As IntPtr,
+                                         ByVal uMsg As WindowsMessages, ByVal wParam As IntPtr,
                                          ByVal lParam As IntPtr) As IntPtr
         'CallWindowProc与DefWindowProc关键的区别在于：
         'CallWindowProc 是发送某个消息给某个处理函数(包括自定义消息处理函数),  而DefWindowProc 是发送某个消息给Windows默认的消息处理函数.
@@ -64,8 +64,8 @@ Public Class APIMessage
     ''' <remarks>如果接收消息的窗口是同一应用程序的一部分，那么这个窗口的窗口函数就被作为-个子程序马上被调用。
     ''' 如果接收消息的窗口是被另外的线程所创建的，那么窗口系统就切换到相应的线程并且调用相应的窗口函数，这条消息不放在目标应用程序队列中。
     ''' </remarks>
-    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
-    Public Shared Function SendMessage(ByVal hWnd As IntPtr, _
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Public Shared Function SendMessage(ByVal hWnd As IntPtr,
                                        ByVal Msg As UInteger, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
     End Function
 
@@ -78,9 +78,9 @@ Public Class APIMessage
     ''' <param name="lParam"></param>
     ''' <returns></returns>
     ''' <remarks></remarks>
-    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
-    Public Shared Function PostMessage(ByVal hWnd As IntPtr, _
-                                       ByVal Msg As UInteger, ByVal wParam As IntPtr, _
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)>
+    Public Shared Function PostMessage(ByVal hWnd As IntPtr,
+                                       ByVal Msg As UInteger, ByVal wParam As IntPtr,
                                        ByVal lParam As IntPtr) As Boolean
     End Function
 
@@ -93,8 +93,8 @@ Public Class APIMessage
     ''' <param name="lParam"></param>
     ''' <returns>如消息投递成功，则返回TRUE（非零）。会设置GetLastError</returns>
     ''' <remarks></remarks>
-    <DllImport("user32.dll", EntryPoint:="PostThreadMessageW", ExactSpelling:=True)> _
-    Public Shared Function PostThreadMessage(ByVal idThread As UInt32, _
+    <DllImport("user32.dll", EntryPoint:="PostThreadMessageW", ExactSpelling:=True)>
+    Public Shared Function PostThreadMessage(ByVal idThread As UInt32,
                                              ByVal msg As UInt32, ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Boolean
     End Function
 
@@ -124,11 +124,11 @@ Public Class APIMessage
     ''' GetMessage does not retrieve messages for windows that belong to other threads nor for threads other than the calling thread. 
     ''' Thread messages, posted by the PostThreadmessage function, have a message hWnd value of NULL. 
     ''' </remarks>
-    <DllImport("user32.dll")> _
-    Public Shared Function GetMessage( _
-                                     ByRef lpMsg As MSG, _
-                                     ByVal hWnd As IntPtr, _
-                                     ByVal wMsgFilterMin As UInteger, _
+    <DllImport("user32.dll")>
+    Public Shared Function GetMessage(
+                                     ByRef lpMsg As MSG,
+                                     ByVal hWnd As IntPtr,
+                                     ByVal wMsgFilterMin As UInteger,
                                      ByVal wMsgFilterMax As UInteger) As <MarshalAs(UnmanagedType.Bool)> Boolean
         ' ' ----- 举例 ----------------
         '获取属于程序主线程的所有窗口的所有消息
@@ -168,14 +168,15 @@ Public Class APIMessage
     ''' 返回值表明是否找到了消息。如果有消息可用则返回TRUE,否则返冋FALSE。
     ''' PeekMessage消息不会从队列中删除WM_ PA丨NT消息.该消息在被处理之前一直留在队列中。
     ''' 但是如果WM—PAINT消息中更新区域为NULL,则函数将删除该消息。</remarks>
-    <DllImport("User32.dll", SetLastError:=True)> _
-    Public Shared Function PeekMessage(ByRef message As NativeMessage, ByVal hWnd As IntPtr, _
+    <DllImport("User32.dll", SetLastError:=True)>
+    Public Shared Function PeekMessage(ByRef message As NativeMessage, ByVal hWnd As IntPtr,
                                        ByVal wMsgFilterMin As UInteger, ByVal wMsgFilterMax As UInteger, ByVal wRemoveMsg As PeekMessageParams) _
                                    As <MarshalAs(UnmanagedType.Bool)> Boolean
 
     End Function
 
 #End Region
+
 #Region "  ---  处理消息"
 
     ''' <summary>
@@ -191,7 +192,7 @@ Public Class APIMessage
     ''' <remarks>返回值表明了函数执行结果。若消息被转换（即是指字符消息被发送到应用程序队列）则返回True；否则返回False。
     ''' 函数TranslateMessage并不修改由参数IpMsg指定的消息，它仅仅为由键盘驱动器产生出ASCII字符的键产生WM_CHAR消息。若应用程序为其他目的而处理虚拟控消息时，不应调用函数TranslateMessage.
     ''' 例如，当TranslateMessage函数返回非零值时，用户不应该再调用TranslateMessage函数。</remarks>
-    <DllImport("user32.dll")> _
+    <DllImport("user32.dll")>
     Public Shared Function TranslateMessage(ByRef lpMsg As MSG) As <MarshalAs(UnmanagedType.Bool)> Boolean
     End Function
 
@@ -203,9 +204,106 @@ Public Class APIMessage
     ''' 则lParam参数是一个函数的地址，该函数被用来代替窗口函数而被调用。</param>
     ''' <returns></returns>
     ''' <remarks>返回值给出窗口函数返回的值.它的意义取决于被发送的消息，但返回值通常被忽略。</remarks>
-    <DllImport("user32.dll")> _
+    <DllImport("user32.dll")>
     Public Shared Function DispatchMessage(ByRef lpmsg As MSG) As IntPtr
     End Function
+
+#End Region
+    
+#Region "  ---  鼠标与键盘的消息发送"
+
+    ''' <summary>
+    ''' 向任意进程发送键盘消息
+    ''' </summary>
+    ''' <param name="bVk">按键的虚拟键值，如回车键为vk_return, tab键为vk_tab,可以参考常用模拟键的键值对照表，也可以通过System.Windows.Forms.Keys枚举来查看。</param>
+    ''' <param name="bScan">扫描码，一般不用设置，用0代替就行；</param>
+    ''' <param name="dwFlags">选项标志，如果为keydown则置0即可，如果为keyup则设成数值2，即常数 KEYEVENTF_KEYUP；</param>
+    ''' <param name="dwExtraInfo">一般也是置0即可。</param>
+    ''' <remarks>
+    '''  调用案例1：
+    ''' keybd_event(System.Windows.Forms.Keys.Escape, 0, 0, 0)  ' 按下 ESCAPE键
+    ''' keybd_event(System.Windows.Forms.Keys.NumLock, 0, KEYEVENTF_KEYUP, 0)  ' 按键弹起，其中 KEYEVENTF_KEYUP=2 
+    '''  调用案例2：    模拟按下 'ALT+F4'键
+    ''' keybd_event(18, 0, 0, 0);
+    ''' keybd_event(115, 0, 0, 0);
+    ''' keybd_event(115, 0, KEYEVENTF_KEYUP, 0); 
+    ''' keybd_event(18, 0, KEYEVENTF_KEYUP, 0);
+    '''</remarks>
+    <DllImport("user32.dll")>
+    Public Shared Sub keybd_event(bVk As Byte, bScan As Byte, dwFlags As UInteger, dwExtraInfo As UIntPtr)
+        '声明方式二： Public Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Integer, ByVal dwExtraInfo As Integer)
+        ' Public Const 
+
+        ' 调用案例1：
+        'keybd_event(System.Windows.Forms.Keys.Escape, 0, 0, 0)  ' 按下 ESCAPE键
+        'keybd_event(System.Windows.Forms.Keys.NumLock, 0, KEYEVENTF_KEYUP, 0)  ' 按键弹起
+
+        ' 调用案例2：    模拟按下 'ALT+F4'键
+        'keybd_event(18, 0, 0, 0);
+        'keybd_event(115, 0, 0, 0);
+        'keybd_event(115, 0, KEYEVENTF_KEYUP, 0); 
+        'keybd_event(18, 0, KEYEVENTF_KEYUP, 0);
+
+    End Sub
+
+
+    ''' <summary> 发送鼠标消息 </summary>
+    ''' <param name="dwFlags"> (位编码)要如何操作鼠标。如果不指定 MOUSEEVENTF_ABSOLUTE，则是相对于当前的鼠标位置。 </param>
+    ''' <param name="dx">根据MOUSEEVENTF_ABSOLUTE标志，指定x，y方向的绝对位置或相对位置 </param>
+    ''' <param name="dy">根据MOUSEEVENTF_ABSOLUTE标志，指定x，y方向的绝对位置或相对位置 </param>
+    ''' <param name="dwData">没有使用，直接赋值为0 </param>
+    ''' <param name="dwExtraInfo">没有使用，直接赋值为0 </param>
+    <DllImport("user32.dll")>
+    Public Shared Sub mouse_event(dwFlags As MouseOperation, dx As UInteger, dy As UInteger, dwData As UInteger, dwExtraInfo As UInteger)
+        '程序中我们直接调用mouse_event函数就可以了
+        'mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, 500, 500, 0, 0);
+
+        '1、这里是鼠标左键按下和松开两个事件的组合即一次单击：
+        'mouse_event (MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0 )
+
+        '2、模拟鼠标右键单击事件：
+        'mouse_event (MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0 )
+
+        '3、两次连续的鼠标左键单击事件构成一次鼠标双击事件：
+        'mouse_event (MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0 )
+        'mouse_event (MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0 )
+
+        '4、使用绝对坐标
+        'MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, 500, 500, 0, 0
+
+        '需要说明的是，如果没有使用MOUSEEVENTF_ABSOLUTE，函数默认的是相对于鼠标当前位置的点，如果dx，和dy，用0，0表示，这函数认为是当前鼠标所在的点。５、直接设定绝对坐标并单击
+        'mouse_event(MOUSEEVENTF_LEFTDOWN, X * 65536 / 1024, Y * 65536 / 768, 0, 0);
+        'mouse_event(MOUSEEVENTF_LEFTUP, X * 65536 / 1024, Y * 65536 / 768, 0, 0);
+        '其中X，Y分别是你要点击的点的横坐标和纵坐标
+    End Sub
+
+    <Flags>
+    Public Enum MouseOperation As UInteger
+        ''' <Summary> 移动鼠标 </Summary>
+        MouseEventF_Move = &H1
+        ' 移动鼠标
+        ''' <Summary> 模拟鼠标左键按下 </Summary>
+        MouseEventF_LeftDown = &H2
+        ' 模拟鼠标左键按下
+        ''' <Summary> 模拟鼠标左键抬起 </Summary>
+        MouseEventF_LeftUp = &H4
+        ' 模拟鼠标左键抬起
+        ''' <Summary> 模拟鼠标右键按下 </Summary>
+        MouseEventF_RightDown = &H8
+        ' 模拟鼠标右键按下
+        ''' <Summary> 模拟鼠标右键抬起 </Summary>
+        MouseEventF_RightUp = &H10
+        ' 模拟鼠标右键抬起
+        ''' <Summary> 模拟鼠标中键按下 </Summary>
+        MouseEventF_MiddleDown = &H20
+        ' 模拟鼠标中键按下
+        ''' <Summary> 模拟鼠标中键抬起 </Summary>
+        MouseEventF_MiddleUp = &H40
+        ' 模拟鼠标中键抬起
+        ''' <Summary> 标示是否采用绝对坐标 </Summary>
+        MouseEventF_Absolute = &H8000
+        ' 标示是否采用绝对坐标
+    End Enum
 
 #End Region
 
